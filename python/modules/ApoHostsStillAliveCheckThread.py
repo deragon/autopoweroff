ae163c5523ad6ed2033f3a8def706bb87693d0d7
Hans Deragon <hans@deragon.biz>
2015-11-17 07:42:02 -0500

    python/modules/ApoDeviceObserverThread.py created.

--- python/modules/ApoHostsStillAliveCheckThread.py
+++ python/modules/ApoHostsStillAliveCheckThread.py
@@ -8,7 +8,7 @@ import logging
 class ApoHostsStillAliveCheckThread(threading.Thread):
 
   def __init__(self, hosts):
-    self.logger = logging.getLogger(__name__);
+    self.logger = logging.getLogger(__name__)
     self.logger.info("Initializing Hosts...")
     threading.Thread.__init__(self, name="HostsStillAliveCheckThread")
     self.setDaemon(True)
@@ -17,14 +17,13 @@ class ApoHostsStillAliveCheckThread(threading.Thread):
   def run(self):
     global gHostsStillAlive
     gHostsStillAlive=[]
-    self.logger.info(__name__ + ".run():  Check on " + \
+    self.logger.info(__name__ + ".run():  Check on " +
         str(self.hosts) + " started.")
-    self.finish = False
-    while not self.finish:
+    while True:
       time.sleep(2)
 
       # While testing for hosts, we do not want to initiale to false the
-      # global variable until all tests are completed.  Thus this is why we 
+      # global variable until all tests are completed.  Thus this is why we
       # use a local variable and only set the global variable once all
       # tests are completed.
       hostsStillAlive=[]
@@ -40,13 +39,13 @@ class ApoHostsStillAliveCheckThread(threading.Thread):
           break
 
         # Experimental use of arping.
-        interfacesList=commands.getstatusoutput( \
+        interfacesList=commands.getstatusoutput(
           "route -n | tail --lines=+3 | awk -F \" \" \"{print \$8}\" | sort -u")[1].split()
 
         self.logger.debug("Interfaces to probe for hosts with arp:  " + str(interfacesList))
         for interface in interfacesList:
-          status = commands.getstatusoutput( \
-                    "arping -w 10 -I " + interface + " " + host)[0]
+          status = commands.getstatusoutput(
+            "arping -w 10 -I " + interface + " " + host)[0]
           #self.logger.debug(status)
           #signal = status & 0xFF
           exitcode = (status >> 8) & 0xFF
@@ -54,6 +53,16 @@ class ApoHostsStillAliveCheckThread(threading.Thread):
           if exitcode == 0:
             hostsStillAlive = hostsStillAlive + [ host ]
             break
+
+      # If the list of hosts alive changed, we report it.
+      if hostsStillAlive != gHostsStillAlive:
+        hostsStillAliveSet = set(hostsStillAlive)
+        gHostsStillAliveSet = set(gHostsStillAlive)
+
+        newHostAliveSet = hostsStillAliveSet - gHostsStillAliveSet
+        newHostDeadSet = gHostsStillAliveSet - hostsStillAliveSet
+        self.logger.info("Newly alive:  " + str(newHostAliveSet) + \
+            "  Newly dead:  " + str(newHostDeadSet))
 
       # Now that all the testing is done, we can update the global variable.
       gHostsStillAlive=hostsStillAlive
