32e360632378c600c961c00b28d65a4031471e30
Hans Deragon <hans@deragon.biz>
2015-11-20 16:53:05 -0500

    poDeviceObserverManager implemented in module ApoDeviceObserverThread.

--- python/modules/ApoDeviceObserverThread.py
+++ python/modules/ApoDeviceObserverThread.py
@@ -4,6 +4,59 @@
 import threading
 import logging
 import time
+import re
+import os
+
+class ApoDeviceObserverManager():
+
+  def __init__(self):
+    self.devicesArray   = []
+    self.apoDevObsArray = []
+    self.logger = logging.getLogger(__name__)
+
+    try:
+      spkrRE = re.compile(".*spkr.*")
+      devicePath = None
+      # Ubuntu 06.06 does not have any /dev/input/by-paty,
+      # only /dev/input.
+      for devicePath in ["/dev/input/by-path", "/dev/input"]:
+        if os.path.exists(devicePath):
+          break
+
+      if devicePath is None:
+        sendmsg("WARNING:  No input device detected.  " + \
+                "Will not be able to detect user activity.")
+      else:
+        for path in os.listdir(devicePath):
+          if spkrRE.search(path) is not None:
+            # Ignoring speaker devices.  I do not understand how a device
+            # related to a speaker can be considered an input device.  A
+            # microphone maybe?  Even if this is the case, if a user simply
+            # leave the microphone on, it will always receive some ambient
+            # sound even if the machine is not use.  We cannot make use of
+            # this.  This device is available in Ubuntu 07.04 and 08.10
+            # (2.6.24-17-generic).
+            continue
+
+          path = devicePath + "/" + path
+          self.logger.debug("path = " + path)
+          self.devicesArray.append(path)
+
+    except OSError, oserror:
+      if oserror.errno != errno.ENOENT:  # No such file or directory
+        raise
+
+    for device in self.devicesArray:
+      self.logger.debug("Device = " + device)
+      apoDevObs = ApoDeviceObserverThread(device)
+      apoDevObs.start()
+      self.apoDevObsArray.append(apoDevObs)
+
+  def terminate():
+    for thread in self.apoDevObsArray:
+      thread.terminate()
+
+
 
 class ApoDeviceObserverThread(threading.Thread):
   def __init__(self, sDevice):
