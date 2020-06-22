6d9a9a70fced94f933adb68626d903ec717bd884
Hans deragon <hans@deragon.biz>
2020-06-21 22:01:42 -0400

    Conditions dict. & ApoObserverNoActionTimeRange.
    Introduction of the conditions dictionary in main thread and
    ApoObserverNoActionTimeRange class which is the first and only class
    compatible with the conditions dictionary.  ApoObserverNoActionTimeRange
    replaces the performSleepForNoActionTimeRange() function.

---
+++ python/modules/ApoObserverNoActionTimeRange.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import threading
+import logging
+import time
+import re
+import os
+import errno
+
+import ApoConfig
+from ApoLibrary import *
+
+class ApoObserverNoActionTimeRange(threading.Thread):
+
+  def status(self):
+
+      if self.isInTimeRange:
+        return ( self.isInTimeRange, "✘ Currently in 'No ActionTimeRange'." )
+      else:
+        return ( self.isInTimeRange, "✓ Currently out of 'No ActionTimeRange'." )
+
+
+  def __init__(self, configuration):
+
+    self.isInTimeRange = None
+    self.logger = logging.getLogger("apo.observer.noaction.timerange")
+    self.configuration = configuration
+    threading.Thread.__init__(self, name=__name__)
+
+
+  def run(self):
+
+    self.logger.info(__name__ + ".run():  started.")
+
+    while True:
+
+      # Checking if the current hour is within the "no action range".
+      # Now contains the current hour.
+
+      now = time.time()
+      # self.logger.debug("UTC:  ", time.gmtime(now))
+      # self.logger.debug("LOCAL:  ", time.localtime(now))
+      nowtuple = time.localtime(now)
+      nowhour = nowtuple.tm_hour
+      # self.logger.debug(nowhour)
+
+      previousIsInTimeRange = self.isInTimeRange
+
+      if self.configuration.noactiontimerange[ApoConfig.STARTHOUR] <= \
+         self.configuration.noactiontimerange[ApoConfig.ENDHOUR]:
+        if nowhour >= self.configuration.noactiontimerange[ApoConfig.STARTHOUR] and \
+           nowhour < self.configuration.noactiontimerange[ApoConfig.ENDHOUR]:
+          self.isInTimeRange = True
+        else:
+          self.isInTimeRange = False
+      else:
+        if nowhour >= self.configuration.noactiontimerange[ApoConfig.STARTHOUR] or \
+           nowhour < self.configuration.noactiontimerange[ApoConfig.ENDHOUR]:
+          self.isInTimeRange = True
+        else:
+          self.isInTimeRange = False
+
+      if previousIsInTimeRange != self.isInTimeRange:
+        # State change occurred.  Need to inform about it.
+        if self.isInTimeRange:
+          self.logger.debug("Switched to No Action Time Range.")
+        else:
+          self.logger.debug("Switched out of No Action Time Range.")
+
+      # This thread has a 1s resolution.  It needs to sleep or else it will
+      # loop continuously and consume 100% CPU.
+      time.sleep(1)
